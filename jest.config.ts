34be9a8ff56fe3609a19a485471387b28282b43c
docente.dev <docente.dev@gmail.com>
2022-06-10 07:00:06 -0400

    chore: config coverage 7

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  collectCoverageFrom: ['./api/src/**'],
+  coverageThreshold: {
+    global: {
+      branches: 55,
+      functions: 43,
+      lines: 57,
+      statements: 54,
+    },
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    }
+  }
+}
